1aed3a8dac5a7cc59cee119142019083322ab779
Jean-Philippe Brucker <jean-philippe@linaro.org>
2022-05-13 09:08:54 +0100

    NVIDIA: SAUCE: NOMERGE: Add KVM Arm RME definitions to Linux headers
    Copy the KVM definitions for Arm RME from the development branch.
    Don't merge, they will be added from the periodic Linux header sync.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    (cherry picked from commit 963d576c9bfe96c0a31b0313f47dc62fc993e92d https://git.codelinaro.org/linaro/dcap/qemu.git)
    [ianm: context adjustment for kpset 6.16 commit 052233c47d66f0 - value change of KVM_CAP_ARM_RME commit]
    Signed-off-by: Ian May <ianm@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -663,14 +663,25 @@ struct kvm_enable_cap {
 #define KVM_S390_SIE_PAGE_OFFSET 1
 
 /*
- * On arm64, machine type can be used to request the physical
- * address size for the VM. Bits[7-0] are reserved for the guest
- * PA size shift (i.e, log2(PA_Size)). For backward compatibility,
- * value 0 implies the default IPA size, 40bits.
+ * On arm64, machine type can be used to request both the machine type and
+ * the physical address size for the VM.
+ *
+ * Bits[11-8] are reserved for the ARM specific machine type.
+ *
+ * Bits[7-0] are reserved for the guest PA size shift (i.e, log2(PA_Size)).
+ * For backward compatibility, value 0 implies the default IPA size, 40bits.
  */
+#define KVM_VM_TYPE_ARM_SHIFT		8
+#define KVM_VM_TYPE_ARM_MASK		(0xfULL << KVM_VM_TYPE_ARM_SHIFT)
+#define KVM_VM_TYPE_ARM(_type)		\
+	(((_type) << KVM_VM_TYPE_ARM_SHIFT) & KVM_VM_TYPE_ARM_MASK)
+#define KVM_VM_TYPE_ARM_NORMAL		KVM_VM_TYPE_ARM(0)
+#define KVM_VM_TYPE_ARM_REALM		KVM_VM_TYPE_ARM(1)
+
 #define KVM_VM_TYPE_ARM_IPA_SIZE_MASK	0xffULL
 #define KVM_VM_TYPE_ARM_IPA_SIZE(x)		\
 	((x) & KVM_VM_TYPE_ARM_IPA_SIZE_MASK)
+
 /*
  * ioctls for /dev/kvm fds:
  */
@@ -952,6 +963,7 @@ struct kvm_enable_cap {
 #define KVM_CAP_ARM_EL2 240
 #define KVM_CAP_ARM_EL2_E2H0 241
 #define KVM_CAP_RISCV_MP_STATE_RESET 242
+#define KVM_CAP_ARM_RME 243
 
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
@@ -1603,5 +1615,14 @@ struct kvm_pre_fault_memory {
 	__u64 flags;
 	__u64 padding[5];
 };
+
+/* Available with KVM_CAP_ARM_RME, only for VMs with KVM_VM_TYPE_ARM_REALM  */
+struct kvm_arm_rmm_psci_complete {
+	__u64 target_mpidr;
+	__u32 psci_status;
+	__u32 padding[3];
+};
+
+#define KVM_ARM_VCPU_RMM_PSCI_COMPLETE	_IOW(KVMIO, 0xd6, struct kvm_arm_rmm_psci_complete)
 
 #endif /* __LINUX_KVM_H */
