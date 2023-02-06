8639d054dae2127afe7fad1e673fc322e16a847c
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-02-06 16:52:37 +0000

    NVIDIA: SAUCE: hw/arm/virt: Disable DTB randomness for confidential VMs
    The dtb-randomness feature, which adds random seeds to the DTB, isn't
    really compatible with confidential VMs since it randomizes the Realm
    Initial Measurement. Enabling it is not an error, but it prevents
    attestation. It also isn't useful to a Realm, which doesn't trust host
    input.
    
    Currently the feature is automatically enabled, unless the user disables
    it on the command-line. Change it to OnOffAuto, and automatically
    disable it for confidential VMs, unless the user explicitly enables it.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    (cherry picked from commit a59b75ce6be0cd86e1e2d5a20dcbc381e791e776 https://git.codelinaro.org/linaro/dcap/qemu.git)
    Signed-off-by: Ian May <ianm@nvidia.com>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -153,7 +153,7 @@ struct VirtMachineState {
     bool virt;
     bool ras;
     bool mte;
-    bool dtb_randomness;
+    OnOffAuto dtb_randomness;
     bool second_ns_uart_present;
     OnOffAuto acpi;
     VirtGICType gic_version;
