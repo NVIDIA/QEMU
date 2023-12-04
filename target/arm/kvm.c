25fd1683ca5a0b038ab5d911ed993b8729b37645
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-12-04 18:48:36 +0000

    NVIDIA: SAUCE: target/arm/kvm: Create scratch VM as Realm if necessary
    Some ID registers have a different value for a Realm VM, for example
    ID_AA64DFR0_EL1 contains the number of breakpoints/watchpoints
    implemented by RMM instead of the hardware.
    
    Even though RMM is in charge of setting up most Realm registers, KVM
    still provides GET_ONE_REG interface on a Realm VM to probe the VM's
    capabilities.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    (cherry picked from commit 35c8e7cb786f3701367fe29dd344c89931bb91d0 https://git.codelinaro.org/linaro/dcap/qemu.git)
    Signed-off-by: Ian May <ianm@nvidia.com>

--- target/arm/kvm.c
+++ target/arm/kvm.c
@@ -106,6 +106,7 @@ bool kvm_arm_create_scratch_host_vcpu(int *fdarray,
 {
     int ret = 0, kvmfd = -1, vmfd = -1, cpufd = -1;
     int max_vm_pa_size;
+    int vm_type;
 
     kvmfd = qemu_open_old("/dev/kvm", O_RDWR);
     if (kvmfd < 0) {
@@ -115,8 +116,9 @@ bool kvm_arm_create_scratch_host_vcpu(int *fdarray,
     if (max_vm_pa_size < 0) {
         max_vm_pa_size = 0;
     }
+    vm_type = kvm_arm_rme_vm_type(MACHINE(qdev_get_machine()));
     do {
-        vmfd = ioctl(kvmfd, KVM_CREATE_VM, max_vm_pa_size);
+        vmfd = ioctl(kvmfd, KVM_CREATE_VM, max_vm_pa_size | vm_type);
     } while (vmfd == -1 && errno == EINTR);
     if (vmfd < 0) {
         goto err;
