b2ab2182fae902b8fce1ee966942db7ba1c4082e
Nicolin Chen <nicolinc@nvidia.com>
2024-04-13 00:18:29 +0000

    NVIDIA: SAUCE: backends/iommufd: Introduce iommufd_backend_alloc_hw_queue
    Allocate/free iommufd hw_queue object per viommu.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    (cherry picked from commit 847ab0c4b9a3103332d2071ad0dd191ce2c3012d)
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    [Shameer: aligned to other APIs]
    Signed-off-by: Shameer Kolothum <skolothumtho@nvidia.com>
    (cherry picked from commit f8a13495e02f1220f5006767a312b1d223929509 https://github.com/shamiali2008/qemu-master/tree/sep302025_nvidia_stable-10.1-vcmdq)
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- include/system/iommufd.h
+++ include/system/iommufd.h
@@ -62,6 +62,11 @@ typedef struct IOMMUFDVeventq {
     uint32_t veventq_fd;
 } IOMMUFDVeventq;
 
+typedef struct IOMMUFDHWqueue {
+    IOMMUFDViommu *viommu;
+    uint32_t hw_queue_id;
+} IOMMUFDHWqueue;
+
 bool iommufd_backend_connect(IOMMUFDBackend *be, Error **errp);
 void iommufd_backend_disconnect(IOMMUFDBackend *be);
 
@@ -97,6 +102,11 @@ bool iommufd_backend_alloc_veventq(IOMMUFDBackend *be, uint32_t viommu_id,
                                    uint32_t type, uint32_t depth,
                                    uint32_t *out_veventq_id,
                                    uint32_t *out_veventq_fd, Error **errp);
+
+bool iommufd_backend_alloc_hw_queue(IOMMUFDBackend *be, uint32_t viommu_id,
+                                    uint32_t data_type, uint32_t index,
+                                    uint64_t addr, uint64_t length,
+                                    uint32_t *out_hw_queue_id, Error **errp);
 
 bool iommufd_backend_set_dirty_tracking(IOMMUFDBackend *be, uint32_t hwpt_id,
                                         bool start, Error **errp);
