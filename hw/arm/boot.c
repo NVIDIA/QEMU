dc9efb468ae67e21c6effc98d371f07a3f43e41e
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-04-26 16:11:59 +0100

    NVIDIA: SAUCE: hw/arm/boot: Skip bootloader for confidential guests
    An independent verifier needs to reconstruct the content of guest memory
    in order to attest that it is running trusted code. To avoid having to
    reconstruct the bootloader generated by QEMU, skip this step and jump
    directly to the kernel, with the DTB address in x0 as specified by the
    Linux boot protocol [1].
    
    [1] https://docs.kernel.org/arch/arm64/booting.html
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    (cherry picked from commit 7fd24221e2c996a29eb4665053d38e5fdd5df286 https://git.codelinaro.org/linaro/dcap/qemu.git)
    Signed-off-by: Ian May <ianm@nvidia.com>

--- hw/arm/boot.c
+++ hw/arm/boot.c
@@ -875,7 +875,13 @@ static void do_cpu_reset(void *opaque)
             if (cpu == info->primary_cpu) {
                 AddressSpace *as = arm_boot_address_space(cpu, info);
 
-                cpu_set_pc(cs, info->loader_start);
+                if (info->skip_bootloader)  {
+                    assert(is_a64(env));
+                    env->xregs[0] = info->dtb_start;
+                    cpu_set_pc(cs, info->entry);
+                } else {
+                    cpu_set_pc(cs, info->loader_start);
+                }
 
                 if (!have_dtb(info)) {
                     if (old_param) {
@@ -973,7 +979,8 @@ static ssize_t arm_load_elf(struct arm_boot_info *info, uint64_t *pentry,
 }
 
 static uint64_t load_aarch64_image(const char *filename, hwaddr mem_base,
-                                   hwaddr *entry, AddressSpace *as)
+                                   hwaddr *entry, AddressSpace *as,
+                                   bool skip_bootloader)
 {
     hwaddr kernel_load_offset = KERNEL64_LOAD_ADDR;
     uint64_t kernel_size = 0;
@@ -1025,7 +1032,8 @@ static uint64_t load_aarch64_image(const char *filename, hwaddr mem_base,
              * bootloader, we can just load it starting at 2MB+offset rather
              * than 0MB + offset.
              */
-            if (kernel_load_offset < BOOTLOADER_MAX_SIZE) {
+            if (kernel_load_offset < BOOTLOADER_MAX_SIZE &&
+                !skip_bootloader) {
                 kernel_load_offset += 2 * MiB;
             }
         }
@@ -1109,7 +1117,8 @@ static void arm_setup_direct_kernel_boot(ARMCPU *cpu,
     }
     if (arm_feature(&cpu->env, ARM_FEATURE_AARCH64) && kernel_size < 0) {
         kernel_size = load_aarch64_image(info->kernel_filename,
-                                         info->loader_start, &entry, as);
+                                         info->loader_start, &entry, as,
+                                         info->skip_bootloader);
         is_linux = 1;
         if (kernel_size >= 0) {
             image_low_addr = entry;
@@ -1249,8 +1258,10 @@ static void arm_setup_direct_kernel_boot(ARMCPU *cpu,
         fixupcontext[FIXUP_ENTRYPOINT_LO] = entry;
         fixupcontext[FIXUP_ENTRYPOINT_HI] = entry >> 32;
 
-        arm_write_bootloader("bootloader", as, info->loader_start,
-                             primary_loader, fixupcontext);
+        if (!info->skip_bootloader) {
+            arm_write_bootloader("bootloader", as, info->loader_start,
+                                 primary_loader, fixupcontext);
+        }
 
         if (info->write_board_setup) {
             info->write_board_setup(cpu, info);
--- include/hw/arm/boot.h
+++ include/hw/arm/boot.h
@@ -141,6 +141,12 @@ struct arm_boot_info {
     ARMCPU *primary_cpu;
 
     /*
+     * Instead of starting in a small bootloader that jumps to the kernel,
+     * immediately start in the kernel.
+     */
+    bool skip_bootloader;
+
+    /*
      * Confidential guest boot loads everything into RAM so it can be measured.
      */
     bool confidential;
