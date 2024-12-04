2089057132bb18e7756e42eaa6a0ad90387d218a
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-12-04 15:34:28 +0000

    NVIDIA: SAUCE: kvm: Use kvm_vm_check_extension() where necessary
    The Arm KVM code can return different values from KVM_CHECK_EXTENSION
    depending on the VM type. Use kvm_vm_check_extension() where necessary
    to ensure we get the right response from KVM.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    (cherry picked from commit 8fe082b9c572e541d2bd07aa82f4f6684948b913 https://git.codelinaro.org/linaro/dcap/qemu.git)
    Signed-off-by: Ian May <ianm@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -2479,7 +2479,7 @@ static int kvm_max_vcpus(KVMState *s)
 
 static int kvm_max_vcpu_id(KVMState *s)
 {
-    int ret = kvm_check_extension(s, KVM_CAP_MAX_VCPU_ID);
+    int ret = kvm_vm_check_extension(s, KVM_CAP_MAX_VCPU_ID);
     return (ret) ? ret : kvm_max_vcpus(s);
 }
 
@@ -2750,7 +2750,7 @@ static int kvm_init(AccelState *as, MachineState *ms)
 
 #ifdef TARGET_KVM_HAVE_GUEST_DEBUG
     kvm_has_guest_debug =
-        (kvm_check_extension(s, KVM_CAP_SET_GUEST_DEBUG) > 0);
+        (kvm_vm_check_extension(s, KVM_CAP_SET_GUEST_DEBUG) > 0);
 #endif
 
     kvm_sstep_flags = 0;
