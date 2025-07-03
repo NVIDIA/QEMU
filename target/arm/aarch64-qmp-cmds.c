0dcbb4b24478f52ea4ff669b188fad58b239d2df
Kazuhiro Abe <fj1078ii@aa.jp.fujitsu.com>
2025-07-03 21:08:44 +0900

    NVIDIA: SAUCE: target/arm/aarch64-qmp-cmds: Add Arm CCA support
    Add a QMP command to retrieve CCA capabilities;
    supported on aarch64 platforms only.
    
    Signed-off-by: Kazuhiro Abe <fj1078ii@aa.jp.fujitsu.com>
    (cherry picked from commit ce93295d18cede59d0207b52eadf8c4962755674 https://git.codelinaro.org/linaro/dcap/qemu.git)
    Signed-off-by: Ian May <ianm@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

---
+++ target/arm/aarch64-qmp-cmds.c
@@ -0,0 +1,49 @@
+/*
+ * Support QMP command for AARCH64
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "kvm_arm.h"
+#include "qapi/error.h"
+#include "qapi/qapi-commands-misc-arm.h"
+#include "system/kvm.h"
+
+static CcaMeasurementAlgoList *cca_measurement_algo_sections(void)
+{
+    CcaMeasurementAlgoList *head = NULL, **tail = &head;
+    CcaMeasurementAlgo *malgo;
+
+    malgo = g_new0(CcaMeasurementAlgo, 1);
+    malgo->measurement_algo = g_malloc(8);
+    memcpy(malgo->measurement_algo, "sha256", 7);
+    QAPI_LIST_APPEND(tail, malgo);
+
+    malgo = g_new0(CcaMeasurementAlgo, 1);
+    malgo->measurement_algo = g_malloc(8);
+    memcpy(malgo->measurement_algo, "sha512", 7);
+    QAPI_LIST_APPEND(tail, malgo);
+
+    return head;
+}
+
+CcaCapability *qmp_query_cca_capabilities(Error **errp)
+{
+    CcaCapability *info = NULL;
+
+    if (!kvm_enabled()) {
+        error_setg(errp, "KVM not enabled");
+        return NULL;
+    }
+
+    if (!kvm_check_extension(kvm_state, KVM_CAP_ARM_RME)) {
+        error_setg(errp, "RME is not enabled in KVM");
+        return NULL;
+    }
+
+    info = g_new0(CcaCapability, 1);
+    info->sections = cca_measurement_algo_sections();
+
+    return info;
+}
+
