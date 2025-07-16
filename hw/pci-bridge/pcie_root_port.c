0b5c539eeaacbb687a9febcb643e4ac22cc3a86d
Yishai Hadas <yishaih@nvidia.com>
2025-07-16 10:35:07 +0300

    NVIDIA: SAUCE: Expose ACS caps on Guest as configured by the HYP
    To support P2P on Guest we must expose to the guest OS the actual PCIe
    topology and configuration as set by the HYP.
    
    Otherwise, the behavior is considered as un-defined.
    
    It might fail by SW or HW.
    
    Extend both root port and downstream port to get acs caps that should
    match the HYP and use them in the guest.
    
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>
    Signed-off-by: Tushar Dave <tdave@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Mitchell Augustin <mitchell.augustin@canonical.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    (backported from commit 78accd69dfa51844ff414efa002983b606fd9439 https://github.com/NVIDIA/QEMU/commits/nvidia_stable-10.1)
    [tdave: context adjustment in hw/pci-bridge/pcie_root_port.c rp_realize() and
    rp_props[] due to PCIESlot::disable_acs removed in 11.0; context adjustment
    in include/hw/pci/pcie.h due to pcie_insert_capability() added in 11.0]
    Signed-off-by: Tushar Dave <tdave@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Acked-by: Morgan Hunter <morgan.hunter@canonical.com>
    Acked-by: Mitchell Augustin <mitchell.augustin@canonical.com>
    Acked-by: Keifer Snedeker <keifer.snedeker@canonical.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- hw/pci-bridge/pcie_root_port.c
+++ hw/pci-bridge/pcie_root_port.c
@@ -46,6 +46,7 @@ static void rp_write_config(PCIDevice *d, uint32_t address,
 static void rp_reset_hold(Object *obj, ResetType type)
 {
     PCIDevice *d = PCI_DEVICE(obj);
+    PCIEPort *p = PCIE_PORT(d);
     DeviceState *qdev = DEVICE(obj);
 
     rp_aer_vector_update(d);
@@ -53,7 +54,7 @@ static void rp_reset_hold(Object *obj, ResetType type)
     pcie_cap_deverr_reset(d);
     pcie_cap_slot_reset(d);
     pcie_cap_arifwd_reset(d);
-    pcie_acs_reset(d);
+    pcie_acs_reset(d, p->acs_caps);
     pcie_aer_root_reset(d);
     pci_bridge_reset(qdev);
     pci_bridge_disable_base_limit(d);
@@ -118,7 +119,10 @@ static void rp_realize(PCIDevice *d, Error **errp)
     rp_aer_vector_update(d);
 
     if (rpc->acs_offset) {
-        pcie_acs_init(d, rpc->acs_offset);
+        rc = pcie_acs_init(d, rpc->acs_offset, p->acs_caps, errp);
+        if (rc < 0) {
+            goto err;
+        }
     }
     return;
 
@@ -151,6 +155,7 @@ static void rp_exit(PCIDevice *d)
 static const Property rp_props[] = {
     DEFINE_PROP_BIT(COMPAT_PROP_PCP, PCIDevice, cap_present,
                     QEMU_PCIE_SLTCAP_PCP_BITNR, true),
+    DEFINE_PROP_UINT16("acs-caps", PCIEPort, acs_caps, 0),
 };
 
 static void rp_instance_post_init(Object *obj)
--- include/hw/pci/pcie.h
+++ include/hw/pci/pcie.h
@@ -137,9 +137,9 @@ bool pcie_insert_capability(PCIDevice *dev, uint16_t cap_id, uint8_t cap_ver,
                             uint16_t offset, uint16_t size);
 void pcie_sync_bridge_lnk(PCIDevice *dev);
 
-void pcie_acs_init(PCIDevice *dev, uint16_t offset);
-void pcie_acs_reset(PCIDevice *dev);
-
+int pcie_acs_init(PCIDevice *dev, uint16_t offset, uint16_t ctrl_bits,
+                  Error **errp);
+void pcie_acs_reset(PCIDevice *dev, uint16_t val);
 void pcie_ari_init(PCIDevice *dev, uint16_t offset);
 void pcie_dev_ser_num_init(PCIDevice *dev, uint16_t offset, uint64_t ser_num);
 void pcie_ats_init(PCIDevice *dev, uint16_t offset, bool aligned);
