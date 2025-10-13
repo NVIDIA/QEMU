NVIDIA/QEMU

commit f06ad59b2b7e0cc05983d9016351b37ad5b0fb21

NVIDIA: SAUCE: hw/arm/virt: Move virt_flash_create() to machvirt_init()
For confidential VMs we'll want to skip flash device creation.
Unfortunately, in virt_instance_init() the machine->cgs member has not
yet been initialized, so we cannot check whether confidential guest is
enabled. Move virt_flash_create() to machvirt_init(), where we can
access the machine->cgs member.

Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
(cherry picked from commit a158ed3493938b9d958dd64d3ee43b03030f0f9b https://git.codelinaro.org/linaro/dcap/qemu.git)
Signed-off-by: Ian May <ianm@nvidia.com>
Acked-by: Matthew R. Ochs <mochs@nvidia.com>
Acked-by: Shameer Kolothum <skolothumtho@nvidia.com>
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

hw/arm/virt.c
@@ -2258,6 +2258,8 @@ static void machvirt_init(MachineState *machine)
     unsigned int smp_cpus = machine->smp.cpus;
     unsigned int max_cpus = machine->smp.max_cpus;
 
+    virt_flash_create(vms);
+
     possible_cpus = mc->possible_cpu_arch_ids(machine);
 
     /*
@@ -3578,8 +3580,6 @@ static void virt_instance_init(Object *obj)
     vms->mte = false;
 
     vms->irqmap = a15irqmap;
-
-    virt_flash_create(vms);
 
     vms->oem_id = g_strndup(ACPI_BUILD_APPNAME6, 6);
     vms->oem_table_id = g_strndup(ACPI_BUILD_APPNAME8, 8);

commit 1e8dd4da58d78953526336a730b24ad57859e4c0

NVIDIA: SAUCE: hw/arm/virt.c: Fix pflash and blockdev
Moving flash creation to machvirt_init() broke two workloads:

* libvirt >=10 uses the “-machine …,pflash0=” property, but by the time
  machvirt_init() runs the property lookup has already happened, so
  libvirt could no longer connect its pflash backend.

* Confidential guests (Arm CCA) create the flash DeviceState objects,
  but bail out early before realizing them. When QEMU walks the device
  tree during startup the unrealized pflash triggers
  qdev_assert_realized_properly(), so Realm launch aborts.

Create the flash devices again in virt_instance_init() so the libvirt
property is available at parse time, but defer the CCA policy to
virt_flash_map(). Add a helper, virt_flash_realize(), and call it both
when mapping flash and in the confidential firmware path so the device
is realized even if the firmware is loaded from RAM instead of from the
pflash. This keeps confidential guests from panic-ing while preserving
the libvirt behaviour.

Signed-off-by: Jiandi An <jan@nvidia.com>
[Fixes: f06ad59b2b NVIDIA: SAUCE: hw/arm/virt: Move virt_flash_create() to machvirt_init()]
Signed-off-by: Ian May <ianm@nvidia.com>

hw/arm/virt.c
@@ -1268,14 +1268,28 @@ static PFlashCFI01 *virt_flash_create1(VirtMachineState *vms,
     return PFLASH_CFI01(dev);
 }
 
+/* Create pflash devices early for blockdev property support.
+ * For confidential VMs, devices are created but not mapped. */
 static void virt_flash_create(VirtMachineState *vms)
 {
-    if (virt_machine_is_confidential(vms)) {
+    /* Always create for property registration.
+     * CCA check moved to virt_flash_map() instead. */
+    vms->flash[0] = virt_flash_create1(vms, "virt.flash0", "pflash0");
+    vms->flash[1] = virt_flash_create1(vms, "virt.flash1", "pflash1");
+}
+
+static void virt_flash_realize(PFlashCFI01 *flash, hwaddr size)
+{
+    DeviceState *dev = DEVICE(flash);
+
+    if (dev->realized) {
         return;
     }
 
-    vms->flash[0] = virt_flash_create1(vms, "virt.flash0", "pflash0");
-    vms->flash[1] = virt_flash_create1(vms, "virt.flash1", "pflash1");
+    assert(QEMU_IS_ALIGNED(size, VIRT_FLASH_SECTOR_SIZE));
+    assert(size / VIRT_FLASH_SECTOR_SIZE <= UINT32_MAX);
+    qdev_prop_set_uint32(dev, "num-blocks", size / VIRT_FLASH_SECTOR_SIZE);
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
 }
 
 static void virt_flash_map1(PFlashCFI01 *flash,
@@ -1283,11 +1297,6 @@ static void virt_flash_map1(PFlashCFI01 *flash,
                             MemoryRegion *sysmem)
 {
     DeviceState *dev = DEVICE(flash);
-
-    assert(QEMU_IS_ALIGNED(size, VIRT_FLASH_SECTOR_SIZE));
-    assert(size / VIRT_FLASH_SECTOR_SIZE <= UINT32_MAX);
-    qdev_prop_set_uint32(dev, "num-blocks", size / VIRT_FLASH_SECTOR_SIZE);
-    sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
 
     memory_region_add_subregion(sysmem, base,
                                 sysbus_mmio_get_region(SYS_BUS_DEVICE(dev),
@@ -1308,6 +1317,9 @@ static void virt_flash_map(VirtMachineState *vms,
      */
     hwaddr flashsize = vms->memmap[VIRT_FLASH].size / 2;
     hwaddr flashbase = vms->memmap[VIRT_FLASH].base;
+
+    virt_flash_realize(vms->flash[0], flashsize);
+    virt_flash_realize(vms->flash[1], flashsize);
 
     if (virt_machine_is_confidential(vms)) {
         return;
@@ -1373,6 +1385,10 @@ static bool virt_confidential_firmware_init(VirtMachineState *vms,
     MemoryRegion *fw_ram;
     hwaddr fw_base = vms->memmap[VIRT_FLASH].base;
     hwaddr fw_size = vms->memmap[VIRT_FLASH].size;
+    hwaddr flashsize = fw_size / 2;
+
+    virt_flash_realize(vms->flash[0], flashsize);
+    virt_flash_realize(vms->flash[1], flashsize);
 
     if (!MACHINE(vms)->firmware) {
         return false;
@@ -2319,8 +2335,6 @@ static void machvirt_init(MachineState *machine)
     bool has_ged = !vmc->no_ged;
     unsigned int smp_cpus = machine->smp.cpus;
     unsigned int max_cpus = machine->smp.max_cpus;
-
-    virt_flash_create(vms);
 
     possible_cpus = mc->possible_cpu_arch_ids(machine);
 
@@ -3652,6 +3666,15 @@ static void virt_instance_init(Object *obj)
     vms->oem_id = g_strndup(ACPI_BUILD_APPNAME6, 6);
     vms->oem_table_id = g_strndup(ACPI_BUILD_APPNAME8, 8);
     cxl_machine_init(obj, &vms->cxl_devices_state);
+
+    /*
+     * Create pflash devices early for blockdev property support.
+     * For confidential VMs, the devices are created but won't be
+     * mapped into memory (virt_flash_map checks machine->cgs).
+     * This allows libvirt 10.x to use:
+     *   -machine virt-10.1,pflash0=node,pflash1=node
+     */
+    virt_flash_create(vms);
 }
 
 static const TypeInfo virt_machine_info = {