b42157c36499f3d96f3a16a5f44493b0e1cfac32
Ian May <ianm@nvidia.com>
2025-10-13 11:24:26 -0700

    Revert "NVIDIA: SAUCE: hw/arm/virt: Define MEC as shared or private"
    We added a `shared-mec` property and sent `ARM_RME_CONFIG_MEC` down to KVM on
    every Realm. Kernels that predate the MEC cap reject that ioctl with EINVAL,
    so Realms fail to start unless the host already supports the new config item.
    
    While we’re waiting for the matching kernel support to land, drop this change
    so that default Realm launches continue to work on older kernels. We’ll bring
    the feature back once we can detect MEC support.
    
    This reverts commit a45c36981d4f586d22ba692134a26201a28add14.
    
    Signed-off-by: Ian May <ianm@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- linux-headers/asm-arm64/kvm.h
+++ linux-headers/asm-arm64/kvm.h
@@ -428,7 +428,6 @@ enum {
 /* List of configuration items accepted for KVM_CAP_ARM_RME_CONFIG_REALM */
 #define ARM_RME_CONFIG_RPV			0
 #define ARM_RME_CONFIG_HASH_ALGO		1
-#define ARM_RME_CONFIG_MEC			2
 
 #define ARM_RME_CONFIG_HASH_ALGO_SHA256		0
 #define ARM_RME_CONFIG_HASH_ALGO_SHA512		1
@@ -448,10 +447,6 @@ struct arm_rme_config {
 			__u32	hash_algo;
 		};
 
-		/* cfg == ARM_RME_CONFIG_MEC */
-		struct {
-			__u32	shared_mec;
-		};
 		/* Fix the size of the union */
 		__u8	reserved[256];
 	};
