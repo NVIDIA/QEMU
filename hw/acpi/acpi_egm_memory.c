NVIDIA/QEMU

commit b62790fe50c3582177d7c2ddc53d469951da76bf

NVIDIA: SAUCE: hw/acpi: Populate DSDT with EGM properties
The Qemu code builds the ACPI DSDT for the VM devices. The
Extended GPU Memory (EGM) information such as physical address,
length and proximity domain ID is populated in the DSDT entries
of the GPU devices present in the same socket as the EGM memory.
This is used by the VM NVIDIA driver to determine the EGM properties.
The GPU device is linked with the EGM memory node through the
acpi-egm-memory object.

While building ACPI tables, go through all of the egm-memory objects.
Find the device and the EGM NUMA node association from the objects.
Patch the DSDT to create the GPU device entries and populate with
the corresponding NUMA node properties with DSDT object.

Signed-off-by: Ankit Agrawal <ankita@nvidia.com>
(cherry picked from commit a5eae1e4755ba9ffea4b20a3678a3f4eacfd4542 https://github.com/nvmochs/QEMU/tree/stable101_smmuv3-accel-07212025_egm)
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

hw/acpi/acpi_egm_memory.c
@@ -5,12 +5,18 @@
 
 #include "qemu/osdep.h"
 #include "hw/acpi/acpi_egm_memory.h"
+#include "hw/acpi/aml-build.h"
 #include "hw/boards.h"
+#include "hw/pci/pci_device.h"
 #include "qemu/error-report.h"
+#include "hw/arm/virt.h"
+#include "include/hw/boards.h"
 
 typedef struct AcpiEgmMemoryClass {
     ObjectClass parent_class;
 } AcpiEgmMemoryClass;
+
+static int gpu_id;
 
 OBJECT_DEFINE_TYPE_WITH_INTERFACES(AcpiEgmMemory, acpi_egm_memory,
                    ACPI_EGM_MEMORY, OBJECT,
@@ -68,4 +74,103 @@ static void acpi_egm_memory_class_init(ObjectClass *oc, const void *data)
         acpi_egm_memory_set_pci_device);
     object_class_property_add(oc, "node", "int", NULL,
         acpi_egm_memory_set_node, NULL, NULL);
+}
+
+static void acpi_dsdt_add_gpu(Aml *dev, int32_t devfn, uint64_t egm_mem_base,
+                              uint64_t egm_mem_size, int egm_mem_pxm)
+{
+    Aml *dev_gpu = aml_device("GPU%d", gpu_id++);
+    Aml *pkg = aml_package(3);
+    Aml *pkg1 = aml_package(2);
+    Aml *pkg2 = aml_package(2);
+    Aml *pkg3 = aml_package(2);
+    Aml *dev_pkg = aml_package(2);
+    Aml *UUID;
+
+    aml_append(dev_gpu, aml_name_decl("_ADR", aml_int(PCI_SLOT(devfn) << 16)));
+
+    aml_append(pkg1, aml_string("nvidia,egm-base-pa"));
+    aml_append(pkg1, aml_int(egm_mem_base));
+
+    aml_append(pkg2, aml_string("nvidia,egm-size"));
+    aml_append(pkg2, aml_int(egm_mem_size));
+
+    aml_append(pkg3, aml_string("nvidia,egm-pxm"));
+    aml_append(pkg3, aml_int(egm_mem_pxm));
+
+    aml_append(pkg, pkg1);
+    aml_append(pkg, pkg2);
+    aml_append(pkg, pkg3);
+
+    UUID = aml_touuid("DAFFD814-6EBA-4D8C-8A91-BC9BBF4AA301");
+    aml_append(dev_pkg, UUID);
+    aml_append(dev_pkg, pkg);
+
+    aml_append(dev_gpu, aml_name_decl("_DSD", dev_pkg));
+    aml_append(dev, dev_gpu);
+}
+
+typedef struct DsdtInfo {
+    Aml *dev;
+    int bus;
+} DsdtInfo;
+
+static int build_all_acpi_egm_memory_dsdt(Object *obj, void *opaque)
+{
+    MachineState *ms = MACHINE(qdev_get_machine());
+    AcpiEgmMemory *egm;
+    DsdtInfo *info = opaque;
+    PCIDevice *pci_dev;
+    Object *o;
+    VirtMachineState *vms = VIRT_MACHINE(qdev_get_machine());
+    uint64_t mem_base;
+    int i;
+
+    if (!object_dynamic_cast(obj, TYPE_ACPI_EGM_MEMORY)) {
+        return 0;
+    }
+
+    egm = ACPI_EGM_MEMORY(obj);
+    if (egm->node >= ms->numa_state->num_nodes) {
+        error_printf("%s: Specified node %d is invalid.\n",
+                     TYPE_ACPI_EGM_MEMORY, egm->node);
+        exit(1);
+    }
+
+    o = object_resolve_path_type(egm->pci_dev, TYPE_PCI_DEVICE, NULL);
+    if (!o) {
+        error_printf("%s: Specified device must be a PCI device.\n",
+                     TYPE_ACPI_EGM_MEMORY);
+        exit(1);
+    }
+
+    pci_dev = PCI_DEVICE(o);
+
+    if (info->bus != pci_bus_num(pci_get_bus(pci_dev))) {
+        return 0;
+    }
+
+    mem_base = vms->memmap[VIRT_MEM].base;
+    for (i = 0; i < ms->numa_state->num_nodes; ++i) {
+        if (i == egm->node) {
+            acpi_dsdt_add_gpu(info->dev, pci_dev->devfn, mem_base,
+                              ms->numa_state->nodes[i].node_mem, i);
+            break;
+        }
+
+        if (ms->numa_state->nodes[i].node_mem > 0) {
+            mem_base += ms->numa_state->nodes[i].node_mem;
+        }
+    }
+
+    return 0;
+}
+
+void build_acpi_egm_memory_dsdt(Aml *dev, int bus)
+{
+    DsdtInfo info = {dev, bus};
+
+    object_child_foreach_recursive(object_get_root(),
+                                   build_all_acpi_egm_memory_dsdt,
+                                   &info);
 }

hw/pci-host/gpex-acpi.c
@@ -7,6 +7,7 @@
 #include "hw/pci/pci_bridge.h"
 #include "hw/pci/pcie_host.h"
 #include "hw/acpi/cxl.h"
+#include "hw/acpi/acpi_egm_memory.h"
 
 static void acpi_dsdt_add_pci_route_table(Aml *dev, uint32_t irq,
                                           Aml *scope, uint8_t bus_num)
@@ -171,6 +172,8 @@ void acpi_dsdt_add_gpex(Aml *scope, struct GPEXConfig *cfg)
                                                   cfg->preserve_config);
             }
 
+            build_acpi_egm_memory_dsdt(dev, bus_num);
+
             aml_append(scope, dev);
         }
     }
@@ -245,6 +248,8 @@ void acpi_dsdt_add_gpex(Aml *scope, struct GPEXConfig *cfg)
 
     acpi_dsdt_add_host_bridge_methods(dev, cfg->pci_native_hotplug,
                                       cfg->preserve_config);
+
+    build_acpi_egm_memory_dsdt(dev, 0);
 
     Aml *dev_res0 = aml_device("%s", "RES0");
     aml_append(dev_res0, aml_name_decl("_HID", aml_string("PNP0C02")));

include/hw/acpi/acpi_egm_memory.h
@@ -19,4 +19,6 @@ typedef struct AcpiEgmMemory {
     uint16_t node;
 } AcpiEgmMemory;
 
+void build_acpi_egm_memory_dsdt(Aml *dev, int bus);
+
 #endif

commit fea4044edad3b926cf6cc9af8cdbf38d29db1838

NVIDIA: SAUCE: Reset gpu_id each time building DSDT
There is an assert in build_append_nameseg() that enforces the
length of ACPI segments to 4 characters:

qemu-system-aarch64: hw/acpi/aml-build.c:202: build_append_nameseg: Assertion len <= ACPI_NAMESEG_LEN failed.

This assert can be hit when building the EGM DSDT entries because
the gpu_id can overflow a single character.

To avoid hitting this assert, always reset the gpu_id before building
the DSDT table.

Fixes: b62790fe50c ("NVIDIA: SAUCE: hw/acpi: Populate DSDT with EGM properties")
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
Acked-by: Mitchell Augustin <mitchell.augustin@canonical.com>
Acked-by: Ankit Agrawal <ankita@nvidia.com>
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

hw/acpi/acpi_egm_memory.c
@@ -115,6 +115,11 @@ typedef struct DsdtInfo {
     int bus;
 } DsdtInfo;
 
+void acpi_egm_memory_reset_gpu_id(void)
+{
+    gpu_id = 0;
+}
+
 static int build_all_acpi_egm_memory_dsdt(Object *obj, void *opaque)
 {
     MachineState *ms = MACHINE(qdev_get_machine());

hw/pci-host/gpex-acpi.c
@@ -110,6 +110,9 @@ void acpi_dsdt_add_gpex(Aml *scope, struct GPEXConfig *cfg)
     CrsRangeEntry *entry;
     int i;
 
+    /* Reset GPU ID for each complete ACPI table build */
+    acpi_egm_memory_reset_gpu_id();
+
     /* start to construct the tables for pxb */
     crs_range_set_init(&crs_range_set);
     if (bus) {

include/hw/acpi/acpi_egm_memory.h
@@ -20,5 +20,6 @@ typedef struct AcpiEgmMemory {
 } AcpiEgmMemory;
 
 void build_acpi_egm_memory_dsdt(Aml *dev, int bus);
+void acpi_egm_memory_reset_gpu_id(void);
 
 #endif