204cc237b4fb85f07a36cb441e66c8d3c575719d
Nirmoy Das <nirmoyd@nvidia.com>
2026-04-15 03:28:57 -0700

    Revert "NVIDIA: SAUCE: hw/arm/smmuv3-accel: Make use of get_msi_address_space() callback"
    This reverts commit 081ceaeddb39ed2d7fc5c56709d646b3115e8416.
    
    The get_msi_address_space() approach is superseded by the upstream
    get_msi_direct_gpa() callback which is introduced in the
    preceding patches.
    
    Signed-off-by: Nirmoy Das <nirmoyda@nvidia.com>

--- hw/arm/smmuv3-accel.c
+++ hw/arm/smmuv3-accel.c
@@ -672,26 +672,6 @@ static void smmuv3_accel_unset_iommu_device(PCIBus *bus, void *opaque,
     }
 }
 
-static AddressSpace *smmuv3_accel_find_msi_as(PCIBus *bus, void *opaque,
-                                              int devfn)
-{
-    SMMUState *bs = opaque;
-    SMMUPciBus *sbus = smmu_get_sbus(bs, bus);
-    SMMUv3AccelDevice *accel_dev = smmuv3_accel_get_dev(bs, sbus, bus, devfn);
-    SMMUDevice *sdev = &accel_dev->sdev;
-
-    /*
-     * If the assigned vfio-pci dev has S1 translation enabled by
-     * Guest, return IOMMU address space for MSI translation.
-     * Otherwise, return system address space.
-     */
-    if (accel_dev->s1_hwpt) {
-        return &sdev->as;
-    } else {
-        return &address_space_memory;
-    }
-}
-
 static uint64_t smmuv3_accel_get_msi_gpa(PCIBus *bus, void *opaque, int devfn)
 {
     SMMUState *bs = opaque;
@@ -796,7 +776,6 @@ static const PCIIOMMUOps smmuv3_accel_ops = {
     .get_viommu_flags = smmuv3_accel_get_viommu_flags,
     .set_iommu_device = smmuv3_accel_set_iommu_device,
     .unset_iommu_device = smmuv3_accel_unset_iommu_device,
-    .get_msi_address_space = smmuv3_accel_find_msi_as,
     .get_msi_direct_gpa = smmuv3_accel_get_msi_gpa,
 };
 
