0ac54083b1baee69e79fee8e8cb706ca23489418
Jean-Philippe Brucker <jean-philippe@linaro.org>
2026-08-24 14:13:36 -0600

    NVIDIA: SAUCE: linux-headers: Add RME related definitions
    This is a temporary patch intended for testing purposes only. It provides
    definitions related to supporting Realms by the QEMU-VMM.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    (backported from commit 46fba3f60f1d5e5ddea835bd5cfb6dced1ddcf47 https://gitlab.com/Linaro/cca-public/qemu/-/tree/upstream-v4)
    [ianm: Backport KVM_VM_TYPE_ARM_PROTECTED and
    KVM_VM_TYPE_ARM_MASK definitions missing from QEMU 11.0.]
    Signed-off-by: Ian May <ianm@nvidia.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -677,10 +677,19 @@ struct kvm_enable_cap {
  * address size for the VM. Bits[7-0] are reserved for the guest
  * PA size shift (i.e, log2(PA_Size)). For backward compatibility,
  * value 0 implies the default IPA size, 40bits.
+ *
+ * Bits[30-31] are reserved for the VM type
  */
 #define KVM_VM_TYPE_ARM_IPA_SIZE_MASK	0xffULL
 #define KVM_VM_TYPE_ARM_IPA_SIZE(x)		\
 	((x) & KVM_VM_TYPE_ARM_IPA_SIZE_MASK)
+#define KVM_VM_TYPE_ARM_NORMAL		0
+#define KVM_VM_TYPE_ARM_REALM		(1UL << 30)
+#define KVM_VM_TYPE_ARM_PROTECTED	(1UL << 31)
+#define KVM_VM_TYPE_ARM_MASK		(KVM_VM_TYPE_ARM_IPA_SIZE_MASK | \
+					 KVM_VM_TYPE_ARM_PROTECTED | \
+					 KVM_VM_TYPE_ARM_REALM)
+
 /*
  * ioctls for /dev/kvm fds:
  */
@@ -701,6 +710,8 @@ struct kvm_enable_cap {
 #define KVM_GET_SUPPORTED_CPUID   _IOWR(KVMIO, 0x05, struct kvm_cpuid2)
 #define KVM_GET_EMULATED_CPUID	  _IOWR(KVMIO, 0x09, struct kvm_cpuid2)
 #define KVM_GET_MSR_FEATURE_INDEX_LIST    _IOWR(KVMIO, 0x0a, struct kvm_msr_list)
+
+#define KVM_ARM_RMI_POPULATE	  _IOWR(KVMIO, 0xd7, struct kvm_arm_rmi_populate)
 
 /*
  * Extension capability list.
@@ -966,6 +977,12 @@ struct kvm_enable_cap {
 #define KVM_CAP_GUEST_MEMFD_FLAGS 244
 #define KVM_CAP_ARM_SEA_TO_USER 245
 #define KVM_CAP_S390_USER_OPEREXEC 246
+#define KVM_CAP_S390_KEYOP 247
+#define KVM_CAP_S390_VSIE_ESAMODE 248
+#define KVM_CAP_S390_HPAGE_2G 249
+#define KVM_CAP_PPC_COMPAT_CAPS 250
+#define KVM_CAP_GUEST_MEMFD_MEMORY_ATTRIBUTES 251
+#define KVM_CAP_ARM_RMI 252
 
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
